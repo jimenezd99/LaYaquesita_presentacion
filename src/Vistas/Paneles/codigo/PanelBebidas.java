7c145faa1c811286c1d250cb669fe4d830680621
jimenezd99 <diana.jimenezn99@gmail.com>
2021-04-02 13:24:40 -0700

    Inicio

---
+++ src/Vistas/Paneles/codigo/PanelBebidas.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Vistas.Paneles.codigo;
+
+import java.awt.Color;
+import javax.swing.GroupLayout;
+
+
+public class PanelBebidas extends javax.swing.JPanel  {
+    
+    
+    public PanelBebidas() {
+        setLayout(new GroupLayout(this));
+        this.setSize(1065, 1000);
+        this.setLocation(15, 0);
+        this.setBackground(Color.yellow);
+    }
+    
+}
---
+++ src/Vistas/Paneles/codigo/PanelOrden.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Vistas.Paneles.codigo;
+
+import java.awt.Color;
+import javax.swing.GroupLayout;
+
+/**
+ *
+ * @author La Yaquesita
+ */
+public class PanelOrden extends javax.swing.JPanel {
+
+    public PanelOrden() {
+        setLayout(new GroupLayout(this));
+        this.setSize(785, 1000);
+        this.setLocation(0, 0);
+        this.setBackground(Color.BLUE);
+    }
+}
