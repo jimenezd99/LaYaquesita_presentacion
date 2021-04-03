f8fe342021829c7e75b3a55914ebbfd70f87a511
jimenezd99 <diana.jimenezn99@gmail.com>
2021-04-02 21:44:41 -0700

    Correcciones menores y renombre de packages

---
+++ src/paneles/codigo/TabbedPanelMenu.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paneles.codigo;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author La Yaquesita
+ */
+public class TabbedPanelMenu extends javax.swing.JPanel {
+
+    public TabbedPanelMenu() {
+//        setLayout(new GroupLayout(this));
+        this.setSize(1000, 250);
+        this.setLocation(786, 15);
+        this.setBackground(Color.yellow);
+       
+    }
+
+}
